beb8e0581d7e65b1a5eb4a75245aae000c9a360c
Victor Markov <victor.markov@urbanise.com>
2021-04-05 17:50:14 +0300

    Basic display of city details

--- src/App.js
+++ src/App.js
@@ -5,25 +5,40 @@ import weatherService from './services/WeatherService';
 
 function App() {
   const [coordinates, setCoordinates] = useState({});
+  const [cityDetails, setCityDetails] = useState({});
+  const [weatherData, setWeatherData] = useState([]);
+
   const getLocation = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((data) => {
-        const newCoords = {lat: data.coords.latitude, lon: data.coords.longitude};
-        setCoordinates({lat: data.coords.latitude, lon: data.coords.longitude});
-        weatherService.getData(newCoords).then(response => {
-          console.log(response.data);
-        });
-      }, (err) => {console.log(err)});
+      navigator.geolocation.getCurrentPosition(setLocationCoordinates, (err) => {console.log(err)});
     }
   }
+
+  const setLocationCoordinates = (data) => {
+    const newCoords = {lat: data.coords.latitude, lon: data.coords.longitude};
+    setCoordinates({lat: data.coords.latitude, lon: data.coords.longitude});
+    weatherService.getData(newCoords).then(response => {
+      console.log(response);
+      setCityDetails(response.data.city);
+      setWeatherData(response.data.list);
+    });
+  }
+
   return (
-    <div className="App">
+    <div className="container">
+      <CityDetails details={cityDetails}></CityDetails>
       {coordinates.lat} {coordinates.lon}
       <Button onClick={getLocation}>
         Get Location
       </Button>
     </div>
   );
+}
+
+function CityDetails({details}) {
+  return (<>
+    <h1>{details.name || 'serbia'}</h1>
+  </>)
 }
 
 export default App;
