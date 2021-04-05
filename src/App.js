8fb3db3a22bc47821f867c961ef2b5f8f2be1364
Victor Markov <victor.markov@urbanise.com>
2021-04-05 17:26:35 +0300

    Get location

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from 'react-bootstrap'; 
 
 function App() {
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((data) => {
+        console.log(data.coords);
+      }, (err) => {console.log(err)});
+    }
+  }
   return (
     <div className="App">
+      <Button onClick={getLocation}>
+        Get Location
+      </Button>
     </div>
   );
 }
