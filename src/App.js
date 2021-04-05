8efb4937b0e274066219d4a00414f7b31377e00d
Victor Markov <victor.markov@urbanise.com>
2021-04-05 17:38:57 +0300

    Add axios and weather service

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import {Button} from 'react-bootstrap'; 
 import { useState } from 'react';
+import weatherService from './services/WeatherService';
 
 function App() {
   const [coordinates, setCoordinates] = useState({});
   const getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((data) => {
-        setCoordinates({lat: data.coords.latitude, lon: data.coords.longitude})
+        const newCoords = {lat: data.coords.latitude, lon: data.coords.longitude};
+        setCoordinates({lat: data.coords.latitude, lon: data.coords.longitude});
+        weatherService.getData(newCoords).then(response => {
+          console.log(response.data);
+        });
       }, (err) => {console.log(err)});
     }
   }
---
+++ src/services/WeatherService.js
@@ -0,0 +1,28 @@
+import axios  from "axios";
+const API_BASE = "http://api.openweathermap.org/data";
+const API_VERSION = "2.5";
+const axiosInstance = axios.create({
+  baseURL: `${API_BASE}/${API_VERSION}`,
+});
+
+class WeatherService {
+  constructor() {
+    this.defaultServiceParams = {
+      appid: "4f751d5b21a82a961ae0933754db09cc",
+      units: "metric",
+    };
+  }
+
+  getData = (coordinates) => {
+    if (coordinates.lat && coordinates.lon) {
+      return axiosInstance.get(`/forecast`, {
+        params: { ...this.defaultServiceParams, ...coordinates },
+      });
+    } else {
+      return Promise.reject("No data provided");
+    }
+  };
+}
+
+const weatherService = new WeatherService();
+export default weatherService;
