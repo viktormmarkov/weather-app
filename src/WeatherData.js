6bacb3d1bb08ab1c0217c956b0d7bedb9c411211
Victor Markov <victor.markov@urbanise.com>
2021-04-05 19:27:20 +0300

    Code formatting

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -25,30 +25,50 @@ function WeatherData({ weatherData = [] }) {
     <Tabs activeKey={key} onSelect={(k) => setKey(k)}>
       {_.keys(weatherDataGrouped).map((d) => (
         <Tab title={getTabName(d)} eventKey={d} key={d}>
-          <WeatherDataDetails weatherDataDetails={weatherDataGrouped[d]}></WeatherDataDetails>
+          <WeatherDataDetails
+            weatherDataDetails={weatherDataGrouped[d]}
+          ></WeatherDataDetails>
         </Tab>
       ))}
     </Tabs>
   );
 }
-function Temp ({temp}) {
-  return (<span>{Math.round(temp)}°</span>)
+function Temp({ temp }) {
+  return <span>{Math.round(temp)}°</span>;
 }
 
-function TempMinMax({tempMin, tempMax, className}) {
-  return (<span className={className ? className : ''}><Temp temp={tempMin}/> | <Temp temp={tempMax}/> </span>)
+function TempMinMax({ tempMin, tempMax, className }) {
+  return (
+    <span className={className ? className : ""}>
+      <Temp temp={tempMin} /> | <Temp temp={tempMax} />{" "}
+    </span>
+  );
 }
 
-function HourDetails({details, className}) {
-  return (<Col>
-    <h6>{DateTime.fromSeconds(details.dt).toLocaleString(DateTime.TIME_24_SIMPLE)}</h6>
-    <TempMinMax className="d-block"tempMin={details.main.temp_min} tempMax={details.main.temp_max}/>
-  </Col>)
+function HourDetails({ details, className }) {
+  return (
+    <Col>
+      <h6>
+        {DateTime.fromSeconds(details.dt).toLocaleString(
+          DateTime.TIME_24_SIMPLE
+        )}
+      </h6>
+      <TempMinMax
+        className="d-block"
+        tempMin={details.main.temp_min}
+        tempMax={details.main.temp_max}
+      />
+    </Col>
+  );
 }
 
 function WeatherDataDetails({ weatherDataDetails }) {
-  const tempMin = _(weatherDataDetails).map(d => d.main.temp_min).min();
-  const tempMax = _(weatherDataDetails).map(d => d.main.temp_max).max();
+  const tempMin = _(weatherDataDetails)
+    .map((d) => d.main.temp_min)
+    .min();
+  const tempMax = _(weatherDataDetails)
+    .map((d) => d.main.temp_max)
+    .max();
   return (
     <>
       <Row>
@@ -57,7 +77,7 @@ function WeatherDataDetails({ weatherDataDetails }) {
           <hr></hr>
           <div>
             <h6>&nbsp;</h6>
-            <TempMinMax tempMin={tempMin} tempMax={tempMax}/>
+            <TempMinMax tempMin={tempMin} tempMax={tempMax} />
           </div>
         </Col>
         <Col>
@@ -65,9 +85,10 @@ function WeatherDataDetails({ weatherDataDetails }) {
           <hr></hr>
           <div>
             <Row>
-            {weatherDataDetails.map(d => (<HourDetails className="px-1" details={d}/>))}
-          </Row>
-
+              {weatherDataDetails.map((d) => (
+                <HourDetails className="px-1" details={d} />
+              ))}
+            </Row>
           </div>
         </Col>
       </Row>
